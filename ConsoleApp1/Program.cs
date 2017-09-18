486d50585722d22c85896f615768123e9dd19d69
mahesh <maheshkumardigarse@gmail.com>
2017-09-18 15:00:28 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+            Console.ReadLine();
+        }
+
+        public static string CreateMessage()
+        {
+            
+            return "Hello World!!";
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("HelloWorld!!", Program.CreateMessage());
+        }
+    }
+}
